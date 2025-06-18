1be113fb3b7ffc996bd759411086d7e79fc9a141
daniel camacho <daniel.camacho@uniat.edu.mx>
2025-06-18 09:41:56 -0600

    +funcion promedio

---
+++ promedio.h
@@ -0,0 +1,24 @@
+#ifndef PROMEDIO_H
+#define PROYECTO_H
+
+float funxPromedio(float numNuevo, float promedioAnterior){
+	float Promedio=(numNuevo+promedioAnterior)/2;
+	return Promedio;
+	};
+
+#endif
+
+//recibir mat
+/*
+//test
+int main()
+{
+	float a;
+	float b;
+	std::cout<<"prom anterior\n>";
+	std::cin>>b;
+	std::cout<<"calif nueva\n>";
+	std::cin>>a;
+	std::cout<<"prom:\n"<<funxPromedio(a, b);
+}
+*/
